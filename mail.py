a7c7947bdffe162a399fd58a4e06f0c6c674600f
IndhumathyChelliah <63176399+IndhumathyChelliah@users.noreply.github.com>
2020-04-04 22:20:10 -0400

    SMTP-python program

---
+++ mail.py
@@ -0,0 +1,22 @@
+import os
+
+import smtplib
+
+email_address=os.environ.get("email_user")
+email_password=os.environ.get("email_pass")
+
+with smtplib.SMTP('smtp.gmail.com',587) as smtp:
+	smtp.ehlo()
+	smtp.starttls()
+	smtp.ehlo()
+
+	smtp.login(email_address,email_password)
+
+	subject="Hello Welcome to Python"
+
+	body="I love to learn Pyhton"
+
+	msg=f"Subject:{subject}\n\n{body}"
+
+	smtp.sendmail(email_address,"indhumathyblog@gmail.com",msg)
+
