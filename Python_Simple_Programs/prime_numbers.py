8d5fdbff50b0113109aaf6499031ebca690db704
IndhumathyChelliah <63176399+IndhumathyChelliah@users.noreply.github.com>
2020-04-19 18:25:23 -0400

    Python Simple Programs

---
+++ Python_Simple_Programs/prime_numbers.py
@@ -0,0 +1,21 @@
+def prime(limit):
+	if limit>1:
+		for num in range(2,limit+1):
+			for i in range(2,num):
+				if num%i==0:
+					break
+			else:
+				print (num)
+
+     
+		
+
+prime (15)
+
+				
+
+
+			
+		
+		
+		
---
+++ Python_Simple_Programs/simplepath.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+path=Path("Python_Simple_Programs")
+
+print (path.exists())
