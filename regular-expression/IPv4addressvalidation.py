d18ffcadfef2b85e9a886adb9e31ade45690bccd
IndhumathyChelliah <63176399+IndhumathyChelliah@users.noreply.github.com>
2020-04-28 00:25:29 -0400

    re-module Python Programs

---
+++ regular-expression/IPv4addressvalidation.py
@@ -0,0 +1,13 @@
+import re
+
+ip=input("Enter ip address: ")
+
+pattern=re.compile(r"^(25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9])$")
+
+m1=re.search(pattern,ip)
+
+if (m1):
+  print ("valid ip address")
+
+else:
+ print ("invalid ip address")
